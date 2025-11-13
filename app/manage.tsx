6a535cb39eeb042ca6f634d6e48122931bde343b
Pitt <dpittmor@gmail.com>
2025-11-13 22:04:56 +0000

    Adds screens

---
+++ app/manage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function Manage() {
+    return (
+        <View>
+            <Text>Manage</Text>
+        </View>
+    );
+}
