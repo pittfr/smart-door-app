84040182d5d8e3756e4d89555c7d54001fc9dfd9
Pitt <dpittmor@gmail.com>
2025-11-14 00:51:18 +0000

    Adds nativewind to project

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,18 @@
 import { Text, View } from "react-native";
+import "./globals.css";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Text className="text-blue-500">
+                Edit app/index.tsx to edit this screen.
+            </Text>
+        </View>
+    );
 }
